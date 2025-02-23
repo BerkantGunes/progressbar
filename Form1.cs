7b961bd5a418f8314cf6c7674e64f0d64f3c7e0d
Berkant Gunes <gunesberkant96@gmail.com>
2025-02-23 20:16:24 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace progressbar
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int sny;
+        Timer tmr = new Timer();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sny = int.Parse(textBox1.Text);
+
+            tmr.Interval = 1000;
+
+            progressBar1.Maximum = sny;
+
+            tmr.Tick += Tmr_Tick;
+            tmr.Start();
+        }
+
+        private void Tmr_Tick(object sender, EventArgs e)
+        {
+            if(progressBar1.Value<sny)
+            {
+                progressBar1.Value++;
+            }
+            else
+            {
+                tmr.Stop();
+            }
+        }
+    }
+}
